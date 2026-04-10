0e31800e26762a9682f93d849e8c56e738d236b1
jaocbhebbel <jacob.hebbel@gmail.com>
2026-04-09 22:49:41 -0400

    made a mongoose schema for resumes

--- server/src/models/resumes.model.js
+++ server/src/models/resumes.model.js
@@ -3,7 +3,47 @@ import mongoose from 'mongoose';
 // schema for chunk collection
 const ResumeSchema = new mongoose.Schema({
     
-}, {
+    // the pre-chunked resume
+    // note no embedding vectors
+    chunkedResume: {
+        
+        education: {
+            type: [String],
+            default: null
+        },
+        
+        experience: {
+            type: [String],
+            default: null
+        },
+
+        projects: {
+            type: [String],
+            default: null,
+
+        },
+
+        leadership: {
+            type: [String],
+            default: null
+        },
+
+        skills: {
+            type: [String],
+            default: null
+        }
+    },
+
+    // global meta are values
+    // shared across all chunks
+    // of this resume
+    globalMeta: {
+        major: String,
+        year: Number,
+        roles: [String]
+    }
+}, 
+{
     timestamps: {
         createdAt: true,
         updatedAt: true
